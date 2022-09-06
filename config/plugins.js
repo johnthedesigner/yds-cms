17e53398d6019f38d217ad103b34f45eb39c60a2
johnthedesigner <john@johnthedesigner.com>
2022-09-05 23:33:26 -0400

    change default email sender

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
         apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        defaultFrom: "info@yankeedahliasociety.com",
-        defaultReplyTo: "info@yankeedahliasociety.com",
+        defaultFrom: "john@johnthedesigner.com",
+        defaultReplyTo: "john@johnthedesigner.com",
         testAddress: "jlivornese@gmail.com",
       },
     },
