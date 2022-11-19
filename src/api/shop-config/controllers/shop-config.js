5fd004335c1a76fa6d23da66a8831c124b47916c
johnthedesigner <john@johnthedesigner.com>
2022-11-19 11:16:42 -0500

    Added shop configuration controls

---
+++ src/api/shop-config/controllers/shop-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  shop-config controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::shop-config.shop-config');
---
+++ src/api/shop-config/routes/shop-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * shop-config router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::shop-config.shop-config');
